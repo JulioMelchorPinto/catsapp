4c7432fab0d6f85f186e3343a710e1ecbff9674b
Julio César Melchor Pinto <julio.melchor@ciencias.unam.mx>
2020-06-14 23:06:29 -0500

    add main layout with toolbar and content container

--- projects/catsapp-web/src/app/app.module.ts
+++ projects/catsapp-web/src/app/app.module.ts
@@ -1,18 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
+import { MatMenuModule } from "@angular/material/menu";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { HeaderComponent } from './header/header.component';
+import { ContentComponent } from './content/content.component';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      HeaderComponent,
+      ContentComponent
+   ],
+   imports: [
+      BrowserModule,
+      AppRoutingModule,
+      BrowserAnimationsModule,
+      MatToolbarModule,
+      MatIconModule,
+      MatButtonModule,
+      MatMenuModule,
+      FlexLayoutModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
